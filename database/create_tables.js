017dc44c98b361919a6d08c477eb77e1f73998e9
Munyoung Cho <munyoungc31@gmail.com>
2024-02-05 20:17:09 -0800

    fix error

--- database/create_tables.js
+++ database/create_tables.js
@@ -4,7 +4,7 @@ async function createTables() {
   let createUserSQL = `
 		CREATE TABLE IF NOT EXISTS user (
             user_id INT NOT NULL AUTO_INCREMENT,
-            username VARCHAR(25) NOT NULL,
+            username VARCHAR(500) NOT NULL,
             password VARCHAR(100) NOT NULL,
             PRIMARY KEY (user_id),
             UNIQUE INDEX unique_username (username ASC) VISIBLE);
